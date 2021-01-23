0724fca4831011a36d6aa01a226ecc5f2f1f6f3c
Nitish <nitish@pop-os.localdomain>
2021-01-23 19:15:12 +0530

    update

--- final_script.sh
+++ final_script.sh
@@ -1,11 +1,9 @@
-youtube-dl https://www.youtube.com/playlist?list=PLW-dXebzrk5B6PAmm-h2PJz82j6_222Vu --embed-thumbnail -o '%(playlist)s/%(title)s - %(uploader)s - %(id)s.%(ext)s' -f 140 -i --no-continue --no-overwrites --download-archive downloaded.txt --add-metadata > log.txt ;\
-youtube-dl https://www.youtube.com/playlist?list=PLW-dXebzrk5B6PAmm-h2PJz82j6_222Vu -i --flat-playlist --get-id > playlist.txt ;\
-youtube-dl https://www.youtube.com/playlist?list=PLW-dXebzrk5B6PAmm-h2PJz82j6_222Vu --get-id -i -o '%(title)s - %(uploader)s.%(ext)s' --get-filename > check_availability.txt ;\
+youtube-dl https://www.youtube.com/playlist?list=PLW-dXebzrk5B6PAmm-h2PJz82j6_222Vu --embed-thumbnail -o '/home/nitish/%(playlist)s/%(title)s - %(uploader)s - %(id)s.%(ext)s' -f 140 -i --no-continue --no-overwrites --download-archive /home/nitish/ytdl-playlist/downloaded.txt --add-metadata > /home/nitish/ytdl-playlist/log.txt ;\
+youtube-dl https://www.youtube.com/playlist?list=PLW-dXebzrk5B6PAmm-h2PJz82j6_222Vu -i --flat-playlist --get-id > /home/nitish/ytdl-playlist/playlist.txt ;\
+youtube-dl https://www.youtube.com/playlist?list=PLW-dXebzrk5B6PAmm-h2PJz82j6_222Vu --get-id -i -o '%(title)s - %(uploader)s.%(ext)s' --get-filename > /home/nitish/ytdl-playlist/check_availability.txt ;\
 awk '{print $2}' downloaded.txt > output.txt ;\
 grep -vxFf output.txt playlist.txt > missing.txt ;\
 rm output.txt ;\
-cp missing.txt playlist.txt downloaded.txt check_availability.txt log.txt ytdl-playlist/ ;\
-cd ytdl-playlist ;\
 git add . ;\
 git commit -m "update" ;\
 git push origin main
